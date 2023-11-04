921aa9e3877efc40d9fcc0503b7fbd1fbcebadac
amr <amr.elsaeed995@gmail.com>
2023-11-04 21:07:51 +0200

    last commit

--- post/views.py
+++ post/views.py
@@ -4,9 +4,25 @@ from .models import Post
 
 
 class PostList(ListView):
-    model = Post 
-    
+    model = Post  ## in template [ object_list , post_list ]
+    # context_object_name = "all_posts"
+    ordering = ['-created_at']
+    # queryset = Post.objects.filter(active=True)
+    # template_name = 'post/test.html'
 
+    def get_queryset(self):
+        return Post.objects.filter(active=True)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['myname'] = 'amr'
+        return context
+    
+    
+    
+    
+    
+    
 
 class PostDetail(DetailView):
     model = Post
@@ -22,3 +38,5 @@ class PostDelete():
 
 class PostUpdate():
     pass 
+
+
